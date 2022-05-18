25944783f56638685f72ccc9107c7198cbb860c4
chloe kim <88276343+chlo3-kim@users.noreply.github.com>
2022-05-18 11:01:58 -0700

    Create Branch.java

---
+++ Branch.java
@@ -0,0 +1,14 @@
+//branches super class that will hold all the different branches
+
+import java.util.*;
+
+public class Branch{
+  
+  private boolean win;
+  
+  public Branch(){
+    
+  }
+  
+  
+   
