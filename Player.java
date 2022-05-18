0e23acaf52f81d1803df72ed466b59c522398044
chloe kim <88276343+chlo3-kim@users.noreply.github.com>
2022-05-18 10:50:07 -0700

    player class

---
+++ Player.java
@@ -0,0 +1,11 @@
+//player class
+//code for a player object which is what will go through all the different branches of stories
+
+import java.util.*;
+
+public class Player{
+  
+  private String name;
+  private int age;
+  
+  
