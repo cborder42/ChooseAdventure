3121474afaabd010aa20c910168f9d8c0bee24b1
sonjal13 <89711909+sonjal13@users.noreply.github.com>
2022-05-23 13:42:33 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,25 +1,140 @@
 //player class
 //code for a player object which is what will go through all the different branches of stories
-
 import java.util.*;
 
 public class Player{
   
+  
+  //instance variables
   private String name;
   private String location;
   private int age;
   private boolean dead;
+  private boolean dogPet;
+    
   
+  //constructor/s
+  public Player(){
+      location = "Chris's room";
+      name = "Bob";
+      age = 94;
+      dead = true;
+      dogPet = false;
+  }
   public Player(String n, int a){
     location = "Chris's room"; 
     name = n;
     age = a;
     dead = true;
+    dogPet = false;
   }
+  
+  public Player(String l, String n, int a, boolean d){
+    location = l;
+    name = n;
+    age = a;
+    dead = d;
+    dogPet = false;
+  }
+  
+  
+  //accessor methods
+  
+  //returns the player's name
+  public String getName(){
+      return name;
+  
+  }
+  
+  
+  // returns the player's location
+  public String getLoc(){
+      return location;
+  
+  }
+  
+  // returns the player's age
+  public int getAge(){
+      return age;
+  
+  }
+  
+  
+  // returns if the player is living
+  public boolean isLiving(){
+      return dead;
+  
+  }
+  
+  //returns if player has pet the dog
+  public boolean hasPet(){
+     return dogPet();
+  }
+  
+  
+  
+  
+  // mutator methods
+  
+  // sets name of player
+  public void setName(String n){
+      name = n;
+  
+  }
+  
+  // sets the location of player
+  public void setLoc(String l){
+      location = l;
+  
+  }
+  
+  // sets the age of player
+  public void setAge(int a){
+      age = a;
+  
+  }
+  
+  //increases players age by given amount
+  public void ageUp(int up){
+      age+=up;
+  
+  }
+  
+  //decreases players age by given amount
+  public void ageDown(int down){
+      age-=down;
+  
+  }
+  
+  // sets the status of player for death
+  public void setDead(boolean d){
+      dead = d;
+      
+  }
+  
+  // sets player's dead to true
+  public void die(){
+      dead = true;
+  
+  }
+  
+  // sets player's dead to false
+  public void live(){
+      dead = false;
+  
+  }
+  
+  // sets player's to having pet the dog
+  public void petDog(){
+    dogPet = true;
+    
+  }
+  
+  
+  
 }
     
     
-  
   
   
   
