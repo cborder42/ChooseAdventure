408dce84c84f831262e243b6cf01a775680be501
sonjal13 <89711909+sonjal13@users.noreply.github.com>
2022-05-27 12:17:54 -0700

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -1,3 +1,6 @@
+//this just contains some code to run with the GUI class, doesnt really matter if we use this to run it, just an example calling to use
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,7 +12,7 @@ public class PlayGame{
 
   
    public static void main(String[]args){
-   
+      //simply creates a player object w/ default variables and then calls the GUI.startGame(Player p) method
       Player p = new Player();
       
       GUI.startGame(p);
