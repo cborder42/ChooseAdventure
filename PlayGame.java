ab11f4c151aa36748ac22547820b964fea8578a5
sonjal13 <89711909+sonjal13@users.noreply.github.com>
2022-05-27 12:11:42 -0700

    Add files via upload

---
+++ PlayGame.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class PlayGame{
+
+  
+   public static void main(String[]args){
+   
+      Player p = new Player();
+      
+      GUI.startGame(p);
+      
+            
+      
+      
+      
+      
+      
+      
+   
+   }
+
+
+
+
+}
